0e38cc92c3ed954234fe87e4d8b997824936750d
Mate Farkas <y2k.elma@gmail.com>
2024-12-04 14:18:23 +0000

    Autoformat code

--- src/today.cr
+++ src/today.cr
@@ -1,9 +1,9 @@
 require "./*"
 
-#puts day_1(File.read("./inputs/day_1.input"))
-#puts day_1b(File.read("./inputs/day_1.input"))
-#puts day_2(File.read("./inputs/day_2.input"))
-#puts day_2b(File.read("./inputs/day_2.input"))
-#puts day_3(File.read("./inputs/day_3.input"))
-#puts day_3b(File.read("./inputs/day_3.input"))
+# puts day_1(File.read("./inputs/day_1.input"))
+# puts day_1b(File.read("./inputs/day_1.input"))
+# puts day_2(File.read("./inputs/day_2.input"))
+# puts day_2b(File.read("./inputs/day_2.input"))
+# puts day_3(File.read("./inputs/day_3.input"))
+# puts day_3b(File.read("./inputs/day_3.input"))
 puts day_4(File.read("./inputs/day_4.input"))
